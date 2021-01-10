1f4db1699f84258d168c622f18ffc74ad2088193
GYKgamer <41442793+GYKgamer@users.noreply.github.com>
2021-01-10 17:37:57 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# Install-steamos-in-Ubuntu
-New updated installation for steamos
+# SteamOS Ubuntu
+
+![SteamOS Ubuntu](image.png)
+
+SteamOS Ubuntu is a set of scripts to create an Ubuntu-based Steam machine.
+
+It will do the following:
+
+* Create the `steam` user account if it does not exist.
+* Install steam, if it is not installed.
+* Install the Steam Compositor, Steam Mode Switch, and boot splash themes.
+* Configure autologin for the `steam` user account.
+* Configure the default session to the Steam Compositor.
+* Create `reboot-to-[steamos,desktop]-mode` scripts to switch between sessions.
+* Create `steamos-fg` script for games that won't run in the foreground on the SteamOS Compositor.
+* Optionally install the latest graphics drivers for AMD, Nvidia, and Intel GPUs.
+* Optionally install proton fixes.
+
+For best results, this should be run on a fresh installation of
+Ubuntu 18.04 desktop.
+
+## Requirements
+In order to install SteamOS Ubuntu, you should have a fresh installation of
+Ubuntu 18.04 Desktop installed. Other versions may work, but have not been 
+tested.
+
+## Installation
+Installation is very simple. Follow these steps to install SteamOS Ubuntu:
+
+1. Install git:    
+`sudo apt install git -y`
+
+2. Clone or download this repository:    
+`git clone https://github.com/ShadowApex/steamos-ubuntu.git`
+
+3. Run the installation script:    
+`cd steamos-ubuntu`    
+`sudo ./install.sh`
+
+## Advanced Options
+The installation script has several options that you can specify upon installation
+in the form of environment variables. You can specify these options by prefixing
+running the install script with the options you want.
+
+For example, if you want to disable installing OpenSSH and run the script non-
+interactively, you can run this command:
+
+`INCLUDE_OPENSSH=false NON_INTERACTIVE=true sudo ./install.sh`
+
+Here is the list of all the available installation options:
+
+| Option Name          | Default | Description                                              |
+| -------------------- | ------- | -------------------------------------------------------- |
+| `INCLUDE_OPENSSH`    | true    | Whether or not OpenSSH server should be installed        |
+| `INCLUDE_SAKURA`     | true    | Whether or not to install a terminal emulator            |
+| `INCLUDE_PROTONFIX`  | true    | Whether or not to install Protonfix                      |
+| `INCLUDE_GPU_DRIVERS`| true    | Whether or not to install the latest GPU drivers         |
+| `GPU_TYPE`           | auto    | GPU drivers to install. Can be: auto, nvidia, amd, intel |
+| `NON_INTERACTIVE`    | false   | Whether or not to prompt the user during install         |
+| `STEAM_USER`         | steam   | The username of the account to autologin as              |
+
+## FAQ
+
+### How can I switch between desktop mode and SteamOS mode?
+
+After installation, there will not be an easy way to switch between a regular
+Gnome desktop session and Steam. In order to make it easier to switch between
+the two, there are two commands that are installed that will let you switch 
+between the two:
+
+* `reboot-to-desktop-mode` - sets gnome as the default session and reboots
+* `reboot-to-steamos-mode` - sets steam as the default session and reboots
+
+You can access the terminal from Steam by adding a local shortcut for `Sakura`.
+
+### Some games aren't launching correctly in SteamOS mode
+When using the SteamOS compositor, some games start behind the big picture UI and
+no graphics are displayed. The `steamos-fg` script forces such games to be shown 
+in the foreground.
+
+Affected games this script fixes include:
+
+    Dead Cells
+    Dirt Rally
+    Civilization VI
+    The Count Lucanor
+    Mad Max
+
+To fix this, add `steamos-fg %command%` to the launch options for each game you 
+wish to use this script with.
+
+## Attributions
+* Alkazar for [steamos-fg](https://github.com/alkazar/steamos-fg)
+
+## Legal
+The Steam logo and Ubuntu logo are registered trademarks of Valve Corporation
+and Canonical respectively. This project is in no way officially affiliated with
+Valve or Canonical.
---
+++ install.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+
+# Set the defaults. These can be overridden by specifying the value as an
+# environment variable when running this script.
+INCLUDE_OPENSSH="${INCLUDE_OPENSSH:-true}"
+INCLUDE_SAKURA="${INCLUDE_SAKURA:-true}"
+INCLUDE_PROTONFIX="${INCLUDE_PROTONFIX:-false}"
+INCLUDE_GPU_DRIVERS="${INCLUDE_GPU_DRIVERS:-true}"
+GPU_TYPE="${GPU_TYPE:-auto}"
+NON_INTERACTIVE="${NON_INTERACTIVE:-false}"
+STEAM_USER="${STEAM_USER:-steam}"
+export STEAM_USER
+
+# Configure the default versions of the SteamOS packages to use. These generally
+# don't ever need to be overridden.
+STEAMOS_COMPOSITOR_VER="${STEAMOS_COMPOSITOR_VER:-1.35+bsos1_amd64}"
+STEAMOS_MODESWITCH_VER="${STEAMOS_MODESWITCH_VER:-1.10+bsos1_amd64}"
+STEAMOS_PLYMOUTH_VER="${STEAMOS_PLYMOUTH_VER:-0.17+bsos2_all}"
+
+# Ensure the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+	echo "This script must be run with sudo."
+	exit
+fi
+
+# Confirm from the user that it's OK to continue
+if [[ "${NON_INTERACTIVE}" != "true" ]]; then
+	echo "Options:"
+	echo "  OpenSSH:      ${INCLUDE_OPENSSH}"
+	echo "  Terminal:     ${INCLUDE_SAKURA}"
+	echo "  Proton Fixes: ${INCLUDE_PROTONFIX}"
+	echo "  GPU Drivers:  ${INCLUDE_GPU_DRIVERS}"
+	echo "    GPU Type:   ${GPU_TYPE}"
+	echo "  Steam User:   ${STEAM_USER}"
+	echo ""
+	echo "This script will configure a SteamOS-like experience on Ubuntu."
+	read -p "Do you want to continue? [Yy] " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "Starting installation..."
+	else
+		echo "Aborting installation."
+		exit
+	fi
+fi
+
+# Download the packages we need. If we fail at downloading, stop the script.
+set -e
+echo "Downloading SteamOS packages..."
+wget "https://repo.steampowered.com/steamos/pool/main/s/steamos-compositor/steamos-compositor_1.35+bsos1_amd64.deb"
+wget "https://repo.steampowered.com/steamos/pool/main/s/steamos-modeswitch-inhibitor/steamos-modeswitch-inhibitor_1.9.1+bsos1_amd64.deb"
+wget "http://repo.steampowered.com/steamos/pool/main/p/plymouth-themes-steamos/plymouth-themes-steamos_0.17+bsos2_all.deb"
+set +e
+
+# See if there is a 'steam' user account. If not, create it.
+if ! grep "^${STEAM_USER}" /etc/passwd > /dev/null; then
+	echo "Steam user '${STEAM_USER}' not found. Creating it..."
+	adduser --disabled-password --gecos "" "${STEAM_USER}"
+fi
+STEAM_UID=$(grep "^${STEAM_USER}" /etc/passwd | cut -d':' -f3)
+STEAM_GID=$(grep "^${STEAM_USER}" /etc/passwd | cut -d':' -f4)
+echo "Steam user '${STEAM_USER}' found with UID ${STEAM_UID} and GID ${STEAM_GID}"
+
+# Choosing from the guide in Proton (SteamPlay) Wiki https://github.com/ValveSoftware/Proton/wiki/Requirements
+# Install the GPU drivers if it was specified by the user.
+if [[ "${INCLUDE_GPU_DRIVERS}" == "true" ]]; then
+
+	# Autodetect the GPU so we can install the appropriate drivers.
+	if [[ "${GPU_TYPE}" == "auto" ]]; then
+		echo "Auto-detecting GPU..."
+		if lspci | grep -i vga | grep -iq nvidia; then
+			echo "  Detected Nvidia GPU"
+			GPU_TYPE="nvidia"
+		elif lspci | grep -i vga | grep -iq amd; then
+			echo "  Detected AMD GPU"
+			GPU_TYPE="amd"
+		elif lspci | grep -i vga | grep -iq intel; then
+			GPU_TYPE="intel"
+			echo "  Detected Intel GPU"
+		else
+			GPU_TYPE="none"
+			echo "  Unable to determine GPU. Skipping driver install."
+		fi
+	fi
+	
+	# Install the GPU drivers.
+	case "${GPU_TYPE}" in
+		nvidia)
+			echo "Installing the latest Nvidia drivers..."
+			add-apt-repository ppa:graphics-drivers/ppa -y
+			apt update
+			apt install nvidia-driver-415 -y
+			;;
+		amd)
+			echo "Installing the latest AMD drivers..."
+			add-apt-repository ppa:oibaf/graphics-drivers -y
+			apt update
+			apt dist-upgrade -y
+	
+			dpkg --add-architecture i386
+			apt update
+			apt install mesa-vulkan-drivers mesa-vulkan-drivers:i386 -y
+			;;
+		intel)
+			echo "Installing the latest mesa drivers..."
+			add-apt-repository ppa:paulo-miguel-dias/pkppa -y
+			apt update
+			apt dist-upgrade -y
+	
+			dpkg --add-architecture i386
+			apt update
+			apt install mesa-vulkan-drivers mesa-vulkan-drivers:i386 -y
+			;;
+		none)
+			echo "GPU not detected."
+			;;
+		*)
+			echo "Skipping GPU driver installation."
+			;;
+	esac
+fi
+
+# Install steam and steam device support.
+echo "Installing steam..."
+apt update
+apt install steam steam-devices x11-utils -y
+
+# WIP - find a way to enable Steamplay without using Desktop Steam Client. Also maybe find a way to enable Steam Beta with latest Steamplay
+# Enable SteamPlay
+#echo "Enable Steamplay..."
+
+# Enable Protonfix for ease of use with certain games that needs tweaking.
+# https://github.com/simons-public/protonfixes
+# Installing Protonfix for ease of use
+if [[ "${INCLUDE_PROTONFIX}" == "true" ]]; then
+	apt install python-pip python3-pip -y
+	echo "Installing protonfix..."    
+	pip3 install protonfixes --upgrade
+	# Installing cefpython3 for visual progress bar
+	pip install cefpython3
+	# Edit Proton * user_settings.py
+fi
+
+# Install a terminal emulator that can be added from Big Picture Mode.
+if [[ "${INCLUDE_SAKURA}" == "true" ]]; then
+	echo "Installing the sakura terminal emulator..."
+	apt install sakura -y
+fi
+
+# Install openssh-server for remote administration
+if [[ "${INCLUDE_OPENSSH}" == "true" ]]; then
+	echo "Installing OpenSSH Server..."
+	apt install openssh-server -y
+fi
+
+# Enable automatic login. We use 'envsubst' to replace the user with ${STEAM_USER}.
+echo "Enabling automatic login..."
+envsubst < ./conf/custom.conf > /etc/gdm3/custom.conf
+
+# Create our session switching scripts to allow rebooting to the desktop
+echo "Creating reboot to session scripts..."
+envsubst < ./conf/reboot-to-desktop-mode.sh > /usr/local/sbin/reboot-to-desktop-mode
+envsubst < ./conf/reboot-to-steamos-mode.sh > /usr/local/sbin/reboot-to-steamos-mode
+chmod +x /usr/local/sbin/reboot-to-desktop-mode
+chmod +x /usr/local/sbin/reboot-to-steamos-mode
+
+# Create the "steamos-fg" script as a workaround for games like Deadcells with the Steam compositor.
+cp ./conf/steamos-fg.sh /usr/local/sbin/steamos-fg
+chmod +x /usr/local/sbin/steamos-fg
+
+# Create a sudoers rule to allow passwordless reboots between sessions.
+echo "Creating sudoers rules to allow rebooting between sessions..."
+cp ./conf/reboot-sudoers.conf /etc/sudoers.d/steamos-reboot
+chmod 440 /etc/sudoers.d/steamos-reboot
+
+# Install the steamos compositor, modeswitch, and themes
+echo "Configuring the SteamOS boot themes..."
+dpkg -i ./*.deb &>/dev/null
+apt install -f -y
+update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/steamos/steamos.plymouth 100
+update-alternatives --set default.plymouth /usr/share/plymouth/themes/steamos/steamos.plymouth
+
+# Update the grub theme.
+echo 'GRUB_BACKGROUND=/usr/share/plymouth/themes/steamos/steamos_branded.png' | tee -a /etc/default/grub
+update-grub
+
+# Set the X session to use the installed steamos session
+echo "Configuring the default session..."
+cp ./conf/steam-session.conf "/var/lib/AccountsService/users/${STEAM_USER}"
+
+echo ""
+echo "Installation complete! Press ENTER to reboot or CTRL+C to exit"
+read -r
+reboot
